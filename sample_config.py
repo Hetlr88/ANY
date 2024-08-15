2d92a0427215c66d8d4255ce06fa520000689ee6
Hetlr88 <mrhetlr0@gmail.com>
2024-08-15 08:56:41 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -4,13 +4,13 @@ class Config(object):
     # get a token from https://chatbase.com
     CHAT_BASE_TOKEN = os.environ.get("CHAT_BASE_TOKEN", "")
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7445547849:AAHfX4_8TQ57cg71Q_l9ABoa6RfgfPWHgsE")
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    APP_ID = int(os.environ.get("APP_ID", 0987655))
+    API_HASH = os.environ.get("API_HASH","3c56d53943fb09a7700b5a82ad084b4e")
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(str(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(str(x) for x in os.environ.get("AUTH_USERS", "6169288210").split())
     # reg: Procedures
     UTUBE_BOT_USERS = []
     SUPER_DLBOT_USERS = []
